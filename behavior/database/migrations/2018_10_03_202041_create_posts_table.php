1859e7a4380abb40021d3e5fdf6fdfcf777fd90d
Jonas <jonas.feruttci@gmail.com>
2021-11-11 22:38:49 -0300

    Starting With Eloquent_orm

--- behavior/database/migrations/2018_10_03_202041_create_posts_table.php
+++ behavior/database/migrations/2018_10_03_202041_create_posts_table.php
@@ -18,8 +18,6 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('subtitle');
             $table->text('description');
-            $table->dateTime('publish_at');
-            $table->timestamps();
         });
     }
 
